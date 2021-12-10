9a9fc9dc22ac3008ffeacde1d78200f00f64dd54
faiz.j <faiz.j@softwareseni.com>
2021-12-10 23:22:42 +0700

    feat(renjana): fix the themes & bump version to .1.0.3

--- renjana-theme/js/display_tweet.js
+++ renjana-theme/js/display_tweet.js
@@ -2,7 +2,7 @@ function recent_tweets(data) {
     for (i=0; i<4; i++) {
         document.getElementById("tweets").innerHTML =
             document.getElementById("tweets").innerHTML +
-            `<a href="https://twitter.com/_fukuo/status/${data[i].id_str ? data[i].id_str : data[i].id}">
+            `<a href="https://twitter.com/${data[i].user.screen_name}/status/${data[i].id_str ? data[i].id_str : data[i].id}">
                     <div class="tweets">
                         <div class="tweets__user">
                             <img src="${data[i].user.profile_image_url}"> <span>${data[i].user.screen_name}</span>
@@ -17,14 +17,3 @@ function recent_tweets(data) {
 
     document.getElementById("twitter").style.display = 'block';
 }
-
-// if(typeof recent_tweets === "function"){
-//     recent_tweets();
-// } else {
-//     document.querySelector(".is-twitter").innerHTML = `
-//         <div class="wrapper__sidebar--body">
-//             <h2>Unable to load tweets</h2>
-//             <p>Failed to load the data, please make sure to connect Twitter on your settings and make your profile public.</p>
-//         </div>
-//     `;
-// }
